e48236fe1c20760f5e937b01e816eea1cf410040
Suvankar Mazumder <smazumder@gmail.com>
2020-06-21 23:44:49 -0400

    Create single-threaded.py

---
+++ concurrency/single-threaded.py
@@ -0,0 +1,3 @@
+; explore concurrency in Python using this example problem:
+; Given a set of 2D points ({(x0, y0), (x1, y1), etc.}), find the closest pair.
+; This problem has a O(n*log(n)) solution, but to keep things simple, we're going to use the naive O(n^2) solution. 
